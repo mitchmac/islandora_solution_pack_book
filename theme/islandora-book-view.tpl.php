a1810379b9a00af1cb82dea88a6f2abb06b123b6
Nigel Banks <nigel.g.banks@gmail.com>
2012-12-15 14:03:08 +0000

    code

--- theme/islandora-book-view.tpl.php
+++ theme/islandora-book-view.tpl.php
@@ -4,4 +4,9 @@
  *
  */
 ?>
-<div>Book</div>
+<?php if (isset($viewer)): ?>
+  <div id="book-viewer">
+    <?php print $viewer; ?>
+  </div>
+<?php endif; ?>
+<!-- @todo Add table of metadata values -->
