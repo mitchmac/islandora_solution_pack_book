e839c62fff5214c65831618bf1f9c389ae727131
Nigel Banks <nigel.g.banks@gmail.com>
2012-12-16 10:36:23 +0000

    Page view with open sea dragon viewer support.

--- theme/islandora-page.tpl.php
+++ theme/islandora-page.tpl.php
@@ -4,4 +4,22 @@
  *
  */
 ?>
-<div>Page</div>
+<?php
+  print l(t('Return to Book View'), "islandora/object/{$book_object_id}");
+?>
+<?php if (isset($viewer)): ?>
+  <div id="book-page-viewer">
+    <?php print $viewer; ?>
+  </div>
+<?php else: ?>
+  <div id="book-page-image">
+    <?php
+      $params = array(
+        'path' => url("islandora/object/{$object->id}/datastream/JPEG/view"),
+        'attributes' => array()
+      );
+      print theme_image($params);
+    ?>
+  </div>
+<?php endif; ?>
+<!-- @todo Add table of metadata values -->
